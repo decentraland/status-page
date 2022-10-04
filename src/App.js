06ad9d04360a09deffc02b2a82ceaac7ca68425b
Matias Pentreath <pentreathm@decentraland.org>
2022-10-04 15:01:27 -0300

    adding favicon and page description

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { Navbar } from 'decentraland-ui/dist/components/Navbar/Navbar';
 import { Page } from 'decentraland-ui/dist/components/Page/Page';
 import { Footer } from 'decentraland-ui/dist/components/Footer/Footer';
 import { Header } from 'decentraland-ui/dist/components/Header/Header';
-import Menu from 'semantic-ui-react/dist/commonjs/collections/Menu'
+import Menu from 'semantic-ui-react/dist/commonjs/collections/Menu';
+import Helmet from 'react-helmet';
 
 
 
@@ -18,6 +19,12 @@ function App() {
               </> 
             }
         />
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Decentraland Status</title>
+            <link rel="canonical" href="http://decentraland.status.org/" />
+            <meta name="Decentraland status page" content="Helmet application" />
+        </Helmet>
         <Page>
           <Header>Hello Wolrd</Header>
           <p>This is a regular page</p>
