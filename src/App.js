ea2e9297091133972dfcda5ee0537274c2dbb9c1
Manuel Mena <manuelmena1993@gmail.com>
2022-10-06 12:02:30 -0300

    feat: list zone

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Navbar } from 'decentraland-ui/dist/components/Navbar/Navbar';
-import { Page } from 'decentraland-ui/dist/components/Page/Page';
 import { Footer } from 'decentraland-ui/dist/components/Footer/Footer';
 import { Header } from 'decentraland-ui/dist/components/Header/Header';
 import Menu from 'semantic-ui-react/dist/commonjs/collections/Menu';
