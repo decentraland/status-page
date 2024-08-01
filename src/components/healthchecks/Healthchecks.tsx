b6e78dedc5e4f83fd9e2390ab3d86cfad552ab1f
pentreathm <pentreathm@decentraland.org>
2024-08-01 15:43:53 -0300

    feat: adding new sites to the status page (#37)

--- src/components/healthchecks/Healthchecks.tsx
+++ src/components/healthchecks/Healthchecks.tsx
@@ -9,12 +9,18 @@ const Healthchecks: FC = () => {
     <Container>
       <Title title='System Status' />
       <ul className="list-group system-status">
-        <CatalystNetworks />
+        <CatalystNetworks />                
+        <Monitor url={'https://comms-gatekeeper.decentraland.org/status'} name={'Communications Gate Keeper'} />
+        <Monitor url={'https://notifications.decentraland.org/status'} name={'Notifications System'} />
         <Monitor url={'https://nft-api.decentraland.org/health/ready'} name={'Marketplace'} />
         <Monitor url={'https://builder-api.decentraland.org/v1/info'} name={'Builder'} />
         <Monitor url={'https://synapse.decentraland.org/_matrix/client/versions'} name={'Chat'} />
-        <Monitor url={'https://places.decentraland.org/api/status'} name={'Places'} />        
-        <Monitor url={'https://events.decentraland.org/api/status'} name={'Events'} />        
+        <Monitor url={'https://api.decentraland.org/v1/map.png'} name={'Atlas Server'} />
+        <Monitor url={'https://places.decentraland.org/api/status'} name={'Places'} />
+        <Monitor url={'https://events.decentraland.org/api/status'} name={'Events'} />
+        <Monitor url={'https://dcl-lists.decentraland.org/status'} name={'Decentraland Lists'} />    
+        <Monitor url={'https://asset-bundle-converter.decentraland.org/health/live'} name={'Asset Bundle Converter'} />
+        {/* <Monitor url={'https://auth-api.decentraland.org/health/ready'} name={'Auth Server'} />            */}
       </ul>
     </Container>
   );
