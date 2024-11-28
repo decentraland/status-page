47e2f7e829152f3e47e152d73604fb29ea5e6cc8
Kevin Szuchet <kevin.szuchet@decentraland.org>
2024-11-28 11:36:11 +0000

    fix: Camera reel service

--- src/components/healthchecks/Healthchecks.tsx
+++ src/components/healthchecks/Healthchecks.tsx
@@ -25,7 +25,7 @@ const Healthchecks: FC = () => {
         <Monitor url={'https://badges.decentraland.org/status'} name={'Badges Server'} />
         <Monitor url={'https://exploration-games.decentraland.org/api/status'} name={'Exploration Games Server'} />
         <Monitor url={'https://auth-api.decentraland.org/health/ready'} name={'Auth Server'} />
-        <Monitor url={'https://camera-reel-service.decentraland.org//health/live'} name={'Camera Reel Service'} />
+        <Monitor url={'https://camera-reel-service.decentraland.org/health/live'} name={'Camera Reel Service'} />
         <Monitor url={'https://decentraland.org'} name={'Decentraland Domain'} />
       </ul>
     </Container>
